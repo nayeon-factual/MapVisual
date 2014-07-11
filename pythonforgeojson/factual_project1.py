b6bf1c6453d3163adb45c6e2e0b99257d67a6c2b
nayeon-factual <nayeon@factual.com>
2014-07-11 13:22:00 -0700

    added dataScienceToolKit for population density information

--- pythonforgeojson/factual_project1.py
+++ pythonforgeojson/factual_project1.py
@@ -1,14 +1,16 @@
-
 from factual import Factual
+import dstk
 
 KEY = "i9VTDsvooscG7eFQ6ycBX16gwAvLqOVUDv9u2dMh"
 SECRET = "uowo6n31xVVg0UuwVfKJnbxCguCuLM0muJljvFxf"
+
+dstk = dstk.DSTK()
 
 def main():
     factual = Factual(KEY, SECRET)
 
     table = factual.table('places-us')
-    lim = 500
+    lim = 5
     
     q = table.filters({"$and":[{'locality':{'$in':['los angeles', 'boston']}}, {'name': {'$blank': False}}, {'locality': {'$blank': False}}, {'latitude': {'$blank': False}}, {'longitude': {'$blank': False}},{'factual_id': {'$blank': False}}]}).limit(lim).sort('$random_133').select("name,locality,latitude,longitude,factual_id")
 
@@ -23,10 +25,12 @@ def main():
         locality = q.data()[count][u'locality']
 
         coor = '"coordinates":' + '[' + str(lng) + ',' + str(lat) +']},'
-
+        pop_coor = '(' + str(lng) + ', ' + str(lat) +')'
+        pop_den = dstk.coordinates2statistics(pop_coor)[0]['statistics']['population_density']['value']
+        
         typFeat = '{ "type": "Feature",'
         geo = '"geometry": {"type": "Point",' + coor
-        prop = '"properties": {"name": "' + name + '", "locality":"' + locality + '", "factual_id": "' + fid + '"}}'
+        prop = '"properties": {"name": "' + name + '", "locality":"' + locality + '", "factual_id": "' + fid + '", "pop_density": "' + str(pop_den) + '"}}'
         
         if count == lim-1:
             print typFeat + geo + prop
@@ -34,6 +38,8 @@ def main():
             print typFeat + geo + prop + ","
 
     print ']}'
+    
+
 
 if __name__ == '__main__':
   main()
