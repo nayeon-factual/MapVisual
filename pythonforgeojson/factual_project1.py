75ba4da3d2b08fadacb24acba84427ce7c949954
nayeon-factual <nayeon@factual.com>
2014-07-11 14:27:28 -0700

    geoJSON now includes population density, us population, and enviro data. msgTable now shows markers in sparsely populated areas

--- pythonforgeojson/factual_project1.py
+++ pythonforgeojson/factual_project1.py
@@ -10,9 +10,9 @@ def main():
     factual = Factual(KEY, SECRET)
 
     table = factual.table('places-us')
-    lim = 10
+    lim = 400
     
-    q = table.filters({"$and":[{'locality':{'$in':['los angeles', 'boston']}}, {'name': {'$blank': False}}, {'locality': {'$blank': False}}, {'latitude': {'$blank': False}}, {'longitude': {'$blank': False}},{'factual_id': {'$blank': False}}]}).limit(lim).sort('$random_133').select("name,locality,latitude,longitude,factual_id")
+    q = table.filters({"$and":[{'locality':{'$in':['los angeles', 'boston', 'rawlins']}}, {'name': {'$blank': False}}, {'locality': {'$blank': False}}, {'latitude': {'$blank': False}}, {'longitude': {'$blank': False}},{'factual_id': {'$blank': False}}]}).limit(lim).sort('$random_133').select("name,locality,latitude,longitude,factual_id")
 
     print '{ "type": "FeatureCollection","features": ['
 
@@ -25,16 +25,15 @@ def main():
         locality = q.data()[count][u'locality']
 
         coor = '"coordinates":' + '[' + str(lng) + ',' + str(lat) +']},'
-#        pop_coor = '(' + str(lat) + ', ' + str(lng) +')'
+
         pop_den = dstk.coordinates2statistics((lat,lng))[0]['statistics']['population_density']['value']
-#        pop_us = dstk.coordinates2statistics(pop_coor)[0]['statistics']['us_population']['value']
-#        landCover = dstk.coordinates2statistics(pop_coor)[0]['statistics']['land_cover']['value']
-#        print pop_us
-#        print landCover
+        #pop_den sometimes returns value even when in mountain or ocean. pop_us remedies this issue but is limited to US. 
+        pop_us = dstk.coordinates2statistics((lat,lng))[0]['statistics']['us_population']['value']
+        landCover = dstk.coordinates2statistics((lat,lng))[0]['statistics']['land_cover']['value']
         
         typFeat = '{ "type": "Feature",'
         geo = '"geometry": {"type": "Point",' + coor
-        prop = '"properties": {"name": "' + name + '", "locality":"' + locality + '", "factual_id": "' + fid + '", "pop_density": "' + str(pop_den) + '"}}'
+        prop = '"properties": {"name": "' + name + '", "locality":"' + locality + '", "factual_id": "' + fid + '", "pop_density": "' + str(pop_den) + '", "us_pop": "' + str(pop_us) + '", "enviro": "' + landCover + '"}}'
         
         if count == lim-1:
             print typFeat + geo + prop
